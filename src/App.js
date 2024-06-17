9b52cb8d28d544dbc9934ea6fa356445a2fe1d3a
MartinLopezDev <martinxx442@gmail.com>
2024-06-17 19:39:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Button} from 'semantic-ui-react'
+import "./"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        hola mundo
+      </h1>
+      <Button primary>
+          Click me!
+      </Button>
     </div>
-  );
-}
 
-export default App;
+  )
+}
